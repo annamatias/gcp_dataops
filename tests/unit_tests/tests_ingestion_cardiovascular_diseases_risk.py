cb22dbc75dbc1e6b2b3bfb8d56ee0fcb580bf89e
annamatias <annamatiask@gmail.com>
2023-07-18 22:49:23 -0300

    refatorando codigo integraçao continua, acrescentando coverage

--- tests/unit_tests/tests_ingestion_cardiovascular_diseases_risk.py
+++ tests/unit_tests/tests_ingestion_cardiovascular_diseases_risk.py
@@ -2,17 +2,18 @@ import unittest
 import logging
 from pyspark.sql import SparkSession
 
+
 class PySparkTest(unittest.TestCase):
- 
+
     @classmethod
     def suppress_py4j_logging(cls):
-        logger = logging.getLogger(‘py4j’)
+        logger = logging.getLogger("py4j")
         logger.setLevel(logging.WARN)
 
     @classmethod
     def create_testing_pyspark_session(cls):
-        return (SparkSession.builder.master(‘local[2]’).appName(‘my-local-testing-pyspark-context’).enableHiveSupport().getOrCreate())
-    
+        return (SparkSession.builder.master("local[2]").appName("my-local-testing-pyspark-context").enableHiveSupport().getOrCreate())
+
     @classmethod
     def setUpClass(cls):
         cls.suppress_py4j_logging()
@@ -21,3 +22,7 @@ class PySparkTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.spark.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
